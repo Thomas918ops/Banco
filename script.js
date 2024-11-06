5318aa3cb130dd3c055ba38ddab5e5a4b8150604
Thomas <thomaspereiragcolares@gmail.com>
2024-11-06 15:07:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Teste")
+const email = "teste@teste.com"
+const senha = "123456"
+localStorage.setItem(email, senha)
+
+const emailInput = document.getElementById("Email")
+const senhaInput = document.getElementById("Senha")
+const form = document.getElementById("formulario_login")
