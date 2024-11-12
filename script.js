09bab769ef710b2ba1123eeb5c68cfe6d787f76b
Thomas <thomaspereiragcolares@gmail.com>
2024-11-12 15:20:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-const email = "teste@teste.com"
-const senha = "123456"
-localStorage.setItem(email, senha)
+const users=[
+    {
+        "id":1,
+        "email":"thomas@gmail.com",
+        "password":"senha123"
+    },
+    {
+        "id":2,
+        "email":"robesvaldo@gmail.com",
+        "password":"ovos"
+    }
+];
 
-const emailInput = document.getElementById("Email")
-const senhaInput = document.getElementById("Senha")
-const form = document.getElementById("formulario_login")
-form.addEventListener("submit", (event)=>{
-    event.preventDefaultt()
-    
-    const emailDigitado = emailInput.value
-    const senhaDigitado = senhaInput.value
-    console.log(emailDigitado)
-    console.log(senhaDigitado)
-    
-})
+const email_input=document.getElementById('email-login');
+const password_input=document.getElementById('password-login');
+const form=document.getElementById('form-login');
 
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const _email=email_input.value;
+    const _password=password_input.value;
+    const user_data=users.find((user)=>{return user.email==_email && user.password==_password});
+
+    if (user_data!=undefined){
+        Swal.fire({
+            title:'Login sucedido',
+            text:'Acesso ao sistema liberado',
+            icon:'success',
+            showConfirmButton:false,
+            timer:1000
+          });
+    }else{
+        Swal.fire({
+            title:'Falha no Login',
+            text:'Tente novamente',
+            icon:'error',
+            showConfirmButton:false,
+            timer:2000
+          });
+    }
+});
